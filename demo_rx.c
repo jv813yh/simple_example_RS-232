0438f371d18147095ac73cde9bbcfcc0b2c37db8
jv813yh <56434164+jv813yh@users.noreply.github.com>
2022-04-02 15:59:00 +0200

    Update demo_rx.c

--- demo_rx.c
+++ demo_rx.c
@@ -14,12 +14,12 @@
 **************************************************/
 
 /* Basic libraries for working in C. */
-#include <stdlib.h>
+//#include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
-#include <math.h>
+//#include <math.h>
 
-#include <time.h>
+//#include <time.h>
 
 #ifdef _WIN32
 #include <Windows.h>
